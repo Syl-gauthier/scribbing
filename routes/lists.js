a65d7ab7a94c64233d6ebd3727120d09074a20ad
syl-gauthier <sylvain.gauthier.2012@gmail.com>
2017-07-19 03:52:16 +0900

    Add base routing for update and new lists

--- routes/lists.js
+++ routes/lists.js
@@ -12,6 +12,28 @@ router.get('/', function(req, res) {
     res.redirect('/');
   }
 });
+
+router.post('/update', function(req, res) {
+  var data = {_id: req.body.meta._id, name: req.body.meta.name, languages: req.body.languages, words: req.body.words};
+  lists.update(data, function(err, result) {
+    if(err){
+      res.send('An error occured while saving\n' + result);
+    }
+    else {
+      res.send(result);
+    }
+  });
+});
+
+router.get('/get/:listId', function(req, res) {
+  lists.read(req.params.listId, function(err, result) {
+    if(err) res.send('An error occured while fetching your list\n' + result);
+    else {
+      res.send(result);
+    }
+  });
+});
+
 //5956099830e1cf70680f7db8
 router.get('/:listId', function(req, res) {
   lists.read(req.params.listId, function(err, result) {
@@ -19,9 +41,32 @@ router.get('/:listId', function(req, res) {
       res.redirect('/');
     }
     else {
-      res.render('list', {list: result});
+      if(req.user) {
+        res.render('list', {list: result, user: req.user.id});
+      }
+      else {
+        res.redirect('/');
+      }
     }
   });
 });
+
+router.get('/update/:listId', 
+  function(req, res) {
+    if(req.user) {
+      res.render('test', {listId: req.params.listId, user: req.user.id});
+    }
+    else {
+      res.redirect('/');
+    }
+  }
+);
+
+router.get('/new', 
+  function(req,res) {
+    res.render('newList');
+  });
+
+
 
 module.exports = router;
